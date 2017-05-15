e3542388d2d0ab63f975d7c30c9d74ac88424b24
SchTomi <sch.tom93@gmail.com>
2017-05-15 22:20:32 +0200

    Hero Basic Attack System

--- hero.js
+++ hero.js
@@ -6,6 +6,18 @@ class Hero {
         this._life = life > 30 ? 30 : life
     }
 
+    getHP(){
+        return this._life
+    }
+
+    attacked(){
+        this._life--
+    }
+
+    attack(hero){
+        hero.attacked()
+    }
+
 }
 
 module.exports = Hero
--- tests/hero.spec.js
+++ tests/hero.spec.js
@@ -20,4 +20,46 @@ describe('Hero', function() {
             expect(hero._life).to.eql(1);
         });
     })
+
+    describe('#getHP', function() {
+
+        let life
+        let hero
+
+        beforeEach(function() {
+            life = 30
+            hero = new Hero(life)
+        })
+
+        it('should return init life', function() {
+            expect(hero.getHP()).to.eql(life)
+        })
+
+        it('should not let change life', function() {
+            expect(hero.getHP() - 1).to.eql(life - 1)
+            expect(hero.getHP()).to.eql(life)
+        })
+    })
+
+    describe("#attacked", function() {
+
+        it("should decrease life by 1", function() {
+            let life = 30
+            let hero = new Hero(30)
+            hero.attacked()
+            expect(hero.getHP()).to.eql(life-1)
+        })
+    })
+
+    describe("#attack", function() {
+
+        it("should decrease opponent's life by 1", function() {
+            let life = 30
+            let heroA = new Hero(30)
+            let heroB = new Hero(30)
+            heroA.attack(heroB)
+            expect(heroA.getHP()).to.eql(life)
+            expect(heroB.getHP()).to.eql(life-1)
+        })
+    })
 })
